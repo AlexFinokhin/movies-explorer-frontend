4890d482ce5f3c99bf54df98d976156326f65bcb
Alex Finokhin <101180377+AlexFinokhin@users.noreply.github.com>
2023-07-16 21:08:25 +0300

    UPD: настроена кнопка назад

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,8 @@ import { useNavigate, NavLink } from "react-router-dom";
 const NotFound = () => {
   const navigate = useNavigate();
 
-  const handleGoBack = () => {
+  const handleGoBack = (e) => {
+    e.preventDefault();
     navigate(-3);
   };
 
@@ -12,7 +13,7 @@ const NotFound = () => {
       <section className="not-found">
         <h1 className="not-found__title">404</h1>
         <p className="not-found__text">Страница не найдена</p>
-        <NavLink to="#" className="not-found__back" onClick={handleGoBack}>
+        <NavLink className="not-found__back" onClick={handleGoBack}>
           Назад
         </NavLink>
       </section>
