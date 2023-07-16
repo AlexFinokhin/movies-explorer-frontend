67a3add61dc44079c084b9c9c242a78c5e2c9a1d
Alex Finokhin <101180377+AlexFinokhin@users.noreply.github.com>
2023-07-16 21:07:17 +0300

    upd: добавлены константы для экранов

--- src/hooks/useVisibleCount.js
+++ src/hooks/useVisibleCount.js
@@ -3,6 +3,13 @@ import {
   MOVIES_COUNT_12,
   MOVIES_COUNT_8,
   MOVIES_COUNT_5,
+  MOVIES_BREAKPOINT_1279,
+  MOVIES_BREAKPOINT_1223,
+  MOVIES_BREAKPOINT_766,
+  MOVIES_BREAKPOINT_700,
+  MOVIES_BREAKPOINT_643,
+  MOVIES_BREAKPOINT_642,
+  MOVIES_BREAKPOINT_0,
 } from "../utils/constants";
 
 export const useVisibleCount = () => {
@@ -12,13 +19,17 @@ export const useVisibleCount = () => {
     const width = window.innerWidth;
 
     const breakpoints = [
-      { width: 1279, count: MOVIES_COUNT_12 },
-      { width: 767, count: MOVIES_COUNT_8 },
-      { width: 0, count: MOVIES_COUNT_5 },
+      { width: MOVIES_BREAKPOINT_1279, count: MOVIES_COUNT_12 },
+      { width: MOVIES_BREAKPOINT_1223, count: MOVIES_COUNT_8 },
+      { width: MOVIES_BREAKPOINT_766, count: MOVIES_COUNT_8 },
+      { width: MOVIES_BREAKPOINT_700, count: MOVIES_COUNT_5 },
+      { width: MOVIES_BREAKPOINT_643, count: MOVIES_COUNT_8 },
+      { width: MOVIES_BREAKPOINT_642, count: MOVIES_COUNT_5 },
+      { width: MOVIES_BREAKPOINT_0, count: MOVIES_COUNT_5 },
     ];
 
     const { count } = breakpoints.find(
-      (breakpoint) => width > breakpoint.width
+      (breakpoint) => width >= breakpoint.width
     );
 
     return count;
